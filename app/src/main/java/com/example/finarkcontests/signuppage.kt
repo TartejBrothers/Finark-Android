c3f2792e12d27cacfce968859258a87a135fb511
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2024-06-09 13:11:04 +0530

    Added: Sign Up

--- app/src/main/java/com/example/finarkcontests/signuppage.kt
+++ app/src/main/java/com/example/finarkcontests/signuppage.kt
@@ -3,17 +3,17 @@ package com.example.finarkcontests
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
-
 import android.widget.Button
-class MainActivity : AppCompatActivity() {
+class signuppage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val signUpButton: Button = findViewById(R.id.button2)
+        setContentView(R.layout.activity_signuppage)
+        val signUpButton: Button = findViewById(R.id.button)
         signUpButton.setOnClickListener {
-            val intent = Intent(this, signuppage::class.java)
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
 }
+
+
